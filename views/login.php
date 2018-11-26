c0adf5ab89ebda84f1627f579489043ddf5288b4
Mauricio Abisay Lopez Velazquez <programador5@uvp.mx>
2018-11-26 17:51:30 -0600

    Missing jquery script

--- views/login.php
+++ views/login.php
@@ -20,6 +20,7 @@
         </form>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	var googleUser = {};
 	var startApp = function() {
